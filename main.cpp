65d2c489dad69eab8855e4c41aed4d8a571f80a6
Samvel Abrahamyan <samvel1024@gmail.com>
2018-11-14 18:34:08 +0100

    Template

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+
+#ifdef DEBUG
+const bool debug = true;
+#else
+const bool debug = false;
+#endif
+
+using namespace std;
+
+int main(){
+    cerr << "Hello world";
+    return 0;
+}
