b5da2530f13edc6ea62417929c563571b750485f
Samvel Abrahamyan <samvel1024@gmail.com>
2019-01-21 13:54:18 +0100

    Add logging to template

--- test_endless.sh
+++ test_endless.sh
@@ -9,11 +9,19 @@ then
 fi
 ctr=0
 
+if test -z "$2"
+then
+      echo "Testgen input not specified"
+      exit 1
+fi
+ctr=0
+
+
 while true; do
    ctr=$((ctr + 1))
    echo "********TEST $ctr ******";
 
-   echo "100000 1000" | ./cmake-build-debug/testgen > "$1.in"
+   echo $2 | ./cmake-build-debug/testgen > "$1.in"
    cat "$1.in" | ./cmake-build-debug/naive > "$1.expected"
    cat "$1.in" | ./cmake-build-debug/solution > "$1.out"
    diff "$1.out" "$1.expected"
