512363959fec1122e085f5c03a45560e1fe1dc1b
Samvel Abrahamyan <samvel1024@gmail.com>
2019-01-21 19:09:46 +0100

    Fix generator script

--- generate_tests.sh
+++ generate_tests.sh
@@ -15,7 +15,7 @@ fi
 
 mkdir -p $1
 for i in $(seq 1 20); do
-    echo "generating $1"
+    echo "generating $i"
    file="./$1/t$i.in"
    echo "$2" | ./cmake-build-debug/testgen > ${file}
    cat "$file" | ./cmake-build-debug/naive > "./$1/t$i.out"
